03fa90f58dafa35bc89b36edf459479525d9f6ef
Marcel Hoyer <mhoyer@pixelplastic.de>
2022-09-27 11:40:47 +0100

    fix update-images.sh script and update to octoprint 1.8

--- update-images.sh
+++ update-images.sh
@@ -5,14 +5,20 @@
 
 function fetch_tags() {
   image="$1"
-  tags=$(wget -q https://registry.hub.docker.com/v1/repositories/${image}/tags -O - \
-    | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' \
-    | tr '}' '\n' \
-    | awk -F: '{print $3}' \
-    | grep -E '^[.0-9]+$' \
+  docker_response=$(wget -q https://registry.hub.docker.com/v2/repositories/${image}/tags?page_size=30 -O -)
+  tags=$(echo "${docker_response}" \
+    | jq -r '.results[].name' \
+    | grep -E '^[0-9]+\.[0-9]+$' \
     | sort -t "." -k1,1n -k2,2n -k3,3n
   )
   latest_tag=$(echo "$tags" | tail -n1)
+
+  if [ -z "$latest_tag" ]; then
+    echo "${docker_response}"
+    echo
+    echo "Unable to extract a matching tag from response above from https://registry.hub.docker.com/v1/repositories/${image}/tags."
+    exit 1
+  fi
 
   echo "found $1:$latest_tag"
   sed -i "s|image: ${1}:.*|image: ${1}:${latest_tag}|g" docker-compose.yml
